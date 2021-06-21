c54496956bad5b1c00edbfbd87b917ff33cb7520
Akash Jeez <akashit63@gmail.com>
2021-06-21 22:02:56 +0530

    Add files via upload

---
+++ Glitch-Apps/GlitchApp1/app.py
@@ -0,0 +1,33 @@
+__import__ = 'Akashjeez'
+
+try:
+    import os, sys, json, json, time
+    import requests, pandas, numpy
+    import streamlit as st
+    from datetime import datetime, timedelta
+except Exception as ex:
+    st.write(f'** Module Error ** : { ex }')
+	
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+## Use the Full Page Instead of Narrow Central Column.
+st.set_page_config( page_title = 'Ak@$h😎J€€Z', page_icon = '🔥', layout = 'wide', initial_sidebar_state = 'auto' )
+
+st.title(body = 'Ak@$h😎J€€Z')
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+def Execute_Main() -> None:
+    try:
+        st.write(f"** Current Datetime is ** { datetime.now().strftime('%d-%b-%Y') }")
+    except Exception as ex:
+        st.error(f'** Error : ** { ex } ')
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
+
+## Run the Main Code!
+
+if __name__ == '__main__':
+	Execute_Main()
+
+#--------------------------------------------------------------------------------------------------------------------------------------#
